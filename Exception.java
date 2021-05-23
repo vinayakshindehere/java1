adf18f2dac6ed2e012dbd0cac279c8f15c013bc3
manishashah <Vinayak.Shinde@XEBIAINDIA.LOCAL>
2021-05-23 12:54:47 +0530

    first version of exceptions

---
+++ Exception.java
@@ -0,0 +1,10 @@
+package javaPrograms;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Exception occured");
+	}
+
+}
